aa348a253279f49f1204fa4b7683ccde5b83cd19
Steven Dixon <stevendixon112@gmail.com>
2019-06-20 21:04:48 -0400

    created animations

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+var windowWidth = window.innerWidth;
+var incorrectAnswerLimit = 7;
+var knightLocation = [];
+for(var i = 0; i < incorrectAnswerLimit; i++){
+    if(i === 0){
+        knightLocation.push(Math.floor(windowWidth/incorrectAnswerLimit) + 0 - document.getElementById('knight').offsetWidth/3);
+    }else{
+        knightLocation.push(Math.floor(windowWidth/incorrectAnswerLimit) + knightLocation[i-1] - document.getElementById('knight').offsetWidth/3);
+    }  
+}
+//console.log(knightLocation)
+
+var current = 0;
+
+var btn = document.getElementById('move-btn');
+
+btn.addEventListener('click', function(){
+    
+    document.getElementById('knight').style.left = knightLocation[current] + 'px'; 
+    if(current < incorrectAnswerLimit){
+        current+=1;
+    }else{
+        document.getElementById('fire').style.display = 'block';
+        document.getElementById('banner').style.backgroundColor = '#7c6b6c';
+    }
+});
